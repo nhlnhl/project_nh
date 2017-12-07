3294bb25e8efcd70adbff8b2f975cc6249fe466c
Yoonmee <ymhwang119@naver.com>
2017-12-07 09:27:15 +0900

    d

--- search.php
+++ search.php
@@ -18,6 +18,59 @@
     <!-- <link href="search.css" rel="stylesheet"> -->
 
   </head>
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Project NH</a>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+    </div>
+
+
+            <!-- Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+              <!-- Side Widget -->
+              <div class="card my-4">
+                <h5 class="card-header"><?php echo $_SESSION['login_user']; ?></h5>
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <label>Welcome, <?php echo $_SESSION['login_user']; ?>!</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <a class="btn btn-primary" href="write.php">Write</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Search Widget -->
+              <div class="card my-4">
+                <h5 class="card-header">Search</h5>
+                <div class="card-body">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-secondary" type="button" href="search.php">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
 
 
       <!-- Footer -->
