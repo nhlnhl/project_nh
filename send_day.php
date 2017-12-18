4ff984a9b0ae73bb0bb2977af25379eca64ef349
joobong <nhlnhl@naver.com>
2017-12-18 19:52:42 +0900

    marker done

--- send_day.php
+++ send_day.php
@@ -8,32 +8,31 @@
       $money = addslashes($_POST['money']);
       $content = addslashes($_POST['texteditor']);
       $marker = /*addslashes(*/$_POST['markers']/*)*/;
-      $marker = json_decode($marker);
+      $marker = (array)json_decode($marker);
 
       $sql = "SELECT COUNT(*) AS cnt FROM post";
       $result = mysqli_query($bd, $sql);
       $row = mysqli_fetch_assoc($result);
       $post_id = $row['cnt'];
 
-      //$sql = "INSERT INTO day(post_id, day_date, day_country, day_money, day_content) VALUES ($post_id, '{$date}', '{$country}', $money, '{$content}')";
-      //$send = mysqli_query($bd, $sql);
+      $sql = "INSERT INTO day(post_id, day_date, day_country, day_money, day_content) VALUES ($post_id, '{$date}', '{$country}', $money, '{$content}')";
+      $send = mysqli_query($bd, $sql);
 
       $sql = "SELECT COUNT(*) AS cnt FROM day";
       $result = mysqli_query($bd, $sql);
       $row = mysqli_fetch_assoc($result);
       $itinerary_id = $row['cnt'];
 
-      var_dump($marker);
+      //var_dump($marker);
 
       $count = count($marker);
       for($i = 0; $i < $count; $i++)
       {
-        $lat = $marker[$i]['lat'];
-        $lng = $marker[$i]['lng'];
-        $name = $marker[$i]['name'];
-        echo $lat, $lng, $name;
+        $lat = $marker[$i]->lat;
+        $lng = $marker[$i]->lng;
+        $name = $marker[$i]->name;
         $sql = "INSERT INTO marker(itinerary_id, marker_lat, marker_lng, marker_name) VALUES ($itinerary_id, $lat, $lng, '$name')";
-        //$send = mysqli_query($bd, $sql);
+        $send = mysqli_query($bd, $sql);
       }
 /*
       if($send)
