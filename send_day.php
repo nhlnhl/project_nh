9d46047a392efe9eb5c2ee0090905a9ab35cc1cc
joobong <nhlnhl@naver.com>
2017-12-18 11:19:37 +0900

    change some file names

--- send_day.php
+++ send_day.php
@@ -7,8 +7,9 @@
       $country = addslashes($_POST['country']);
       $money = addslashes($_POST['money']);
       $content = addslashes($_POST['texteditor']);
+      $markers = addslashes($_POST['markers']);
 
-      $sql = "SELECT COUNT(*) AS cnt FROM post";
+      $sql = "SELECT COUNT(*) + 1 AS cnt FROM post";
       $result = mysqli_query($bd, $sql);
       $row = mysqli_fetch_assoc($result);
       $post_id = $row['cnt'];
@@ -17,6 +18,13 @@
 
       $sql = "INSERT INTO day(post_id, day_date, day_country, day_money, day_content) VALUES ($post_id, $date, '{$country}', $money, '{$content}')";
       $send = mysqli_query($bd, $sql);
+
+      $sql = "SELECT COUNT(*) + 1 AS cnt FROM day";
+      $result = mysqli_query($bd, $sql);
+      $row = mysqli_fetch_assoc($result);
+      $day_id = $row['cnt'];
+
+      // marker db에 넣기
 
       if($send)
       {
